b930fcba02dd61f5ba79bddf557e581d5826376d
michaelous <michalkapala.pl@gmail.com>
2021-03-19 23:53:09 +0100

    navbar onScroll animation

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,4 +1,4 @@
-import React, {useContext, useState} from 'react';
+import React, {useContext, useState, useEffect} from 'react';
 import cartImg from './images/cart.png'
 import logo from './images/maison.png'
 import {FiltersContext} from "./AppContext";
@@ -6,15 +6,21 @@ import {FlexItem} from "./flexitem";
 import {Cart} from "./cart";
 
 export const Navbar = () => {
+    const [scroll, setScroll] = useState(false);
     const [cart, setCart] = useState(false);
     const {basket} = useContext(FiltersContext);
     const quantity = basket.length;
     const showCart = () => setCart(!cart);
 
+    useEffect(() => {
+        document.addEventListener("scroll", () => {
+            (window.scrollY > 20) ? setScroll(true) : setScroll(false);
+        })
+    });
 
     return (
         <>
-            <FlexItem className={'navbarWrapper'}>
+            <FlexItem className={`navbarWrapper ${scroll ? ' scroll' : ''}`}>
                 <nav className={"navbar"}>
                     <img className={"logo"} alt={"logo"} src={logo}/>
                     <ul className={"navigation"}>
@@ -23,7 +29,7 @@ export const Navbar = () => {
                         <li className={"navItem"}>elements</li>
                         <li className={"navItem cartNavDetails"}>
                             <img className={"cartIcon"} alt={"cart"} onClick={showCart} src={cartImg}/>
-                            <p className={"quantity"}>({quantity})</p>
+                            <p className={"quantity"}>{quantity}</p>
                         </li>
                     </ul>
                 </nav>
