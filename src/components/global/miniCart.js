b85be665a35f528427ca405125a163801c2f54d3
michaelous <michalkapala.pl@gmail.com>
2021-05-14 00:32:14 +0200

    Show proper information when miniCart is empty && block Checkout component

--- src/components/global/miniCart.js
+++ src/components/global/miniCart.js
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState, useEffect} from 'react';
 import {NavLink} from "react-router-dom";
 import {AppContext} from "../context/appContext";
 import {productsHelper} from "../../steps/shop/filters/productsHelper";
@@ -6,13 +6,28 @@ import {ProductCart} from "./productCart";
 
 export const MiniCart = ({cart, setCart}) => {
     const {basket} = useContext(AppContext);
+    const [linkDisabled, setDisabled] = useState(true);
     const closeCart = () => setCart(false);
+
+    useEffect(() => {
+        ((basket.length) > 0) ? setDisabled(false) : setDisabled(true);
+    }, [basket.length]);
+
+    const cartHeader = () => {
+        return <>
+            {linkDisabled && <div>
+                <p className={"cartHeader"}>Your cart is empty</p>
+                <span className={"shrugEmoji"}>¯\_(ツ)_/¯</span>
+            </div>}
+            {!linkDisabled && <p className={"cartHeader"}>Your cart</p>}
+        </>
+    };
 
     return (
         <div className={cart ? 'cart-wrapper active' : 'cart-wrapper'}>
             <div className={"inside-cart"}>
                 <button className={"closeCart"} onClick={closeCart}>x</button>
-                <p className={"cartHeader"}>Your cart</p>
+                {cartHeader()}
                 {basket.map((value, index) => {
                     return <ProductCart product={value}
                                         index={index}
@@ -20,7 +35,8 @@ export const MiniCart = ({cart, setCart}) => {
                 })}
                 <div className={"totalContainer"}>
                     <p className={"totalHeader"}>Subtotal: ${productsHelper.totalPrice(basket)}</p>
-                    <NavLink className={"checkoutBtn"} exact to="/checkout">Go To Checkout</NavLink>
+                    <NavLink className={`checkoutBtn ${linkDisabled ? ' disabled' : ''}`}
+                             exact to="/checkout">{linkDisabled ? 'Add something to cart' : 'Go To Checkout'}</NavLink>
                 </div>
             </div>
         </div>
