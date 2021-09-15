863ad32feb97ca7560572822e98f75a0324c0770
Michal Kapala <michalkapala.pl@gmail.com>
2021-09-15 10:41:00 +0200

    Create history to prevent 404 after refresh

--- src/components/global/routeContext.js
+++ src/components/global/routeContext.js
@@ -1,25 +1,28 @@
 import React, { useState, useReducer } from 'react';
-import { HashRouter as Router, Switch, Route } from 'react-router-dom';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import { defaultCategory, defaultPriceRange, defaultSort, initialPage } from '../consts/default';
-import {AppContext} from "../context/appContext";
-import {Newsletter} from "./newsletter";
-import {Navbar} from "./navbar";
-import {Footer} from "./footer";
-import {Shop} from "../../steps/shop/shop";
-import {Elements} from "../../steps/elements/elements";
-import {Checkout} from "../../steps/checkout/checkout";
-import {localStorageUtil} from "../storage/localStorage";
-import {useMediaQuery} from 'react-responsive'
-import {shopReducer, ADD_PRODUCT, REMOVE_PRODUCT} from '../../store/reducers';
+import { AppContext } from '../context/appContext';
+import { Newsletter } from './newsletter';
+import { Navbar } from './navbar';
+import { Footer } from './footer';
+import { Shop } from '../../steps/shop/shop';
+import { Elements } from '../../steps/elements/elements';
+import { Checkout } from '../../steps/checkout/checkout';
+import { localStorageUtil } from '../storage/localStorage';
+import { useMediaQuery } from 'react-responsive';
+import { shopReducer, ADD_PRODUCT, REMOVE_PRODUCT } from '../../store/reducers';
+import { createBrowserHistory } from 'history';
+
+const history = createBrowserHistory();
 
 export const RouteContext = () => {
     const [sort, setSort] = useState(defaultSort);
     const [page, setPage] = useState(initialPage);
     const [category, setCategory] = useState(defaultCategory);
     const [priceRange, setPriceRange] = useState(defaultPriceRange);
-    const isMobile = useMediaQuery({query: '(max-width: 992px)'});
+    const isMobile = useMediaQuery({ query: '(max-width: 992px)' });
     const basketData = localStorageUtil.getDataByKey('basket') || [];
-    const initialState = {cart: basketData}
+    const initialState = { cart: basketData };
     const [cartState, dispatch] = useReducer(shopReducer, initialState);
 
     const addProductToCart = (product) => {
@@ -43,15 +46,15 @@ export const RouteContext = () => {
 
     return (
         <AppContext.Provider value={combinedContext}>
-            <Router>
-                <Navbar/>
+            <BrowserRouter history={history}>
+                <Navbar />
                 <Switch>
-                    <Route path="/elements" component={Elements}/>
-                    <Route path="/checkout" component={Checkout}/>
-                    <Route path="/" component={Shop}/>
+                    <Route path="/elements" component={Elements} />
+                    <Route path="/checkout" component={Checkout} />
+                    <Route path="/" component={Shop} />
                 </Switch>
-            </Router>
-            <Newsletter/>
+            </BrowserRouter>
+            <Newsletter />
             <Footer/>
         </AppContext.Provider>
     )
